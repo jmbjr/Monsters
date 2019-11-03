93640ac5004911fbdc384accc9475cf1d1949404
John Boyle <boylejm@gmail.com>
2019-11-02 21:40:55 -0700

    add script to quickly create monster lists

--- monsters.sh
+++ monsters.sh
@@ -1,11 +1,8 @@
 echo Updating monsters
 python=/c/ProgramData/Anaconda3/python.exe
 
-$python monsters.py mr > mr
-$python monsters.py hp > hp
-$python monsters.py attacks > attacks
-$python monsters.py resists > resists
-$python monsters.py ac > ac
-$python monsters.py speed > speed
-$python monsters.py defenses > defenses
-$python monsters.py dragons > dragons
+for file in mr hp attacks resists ac speed defenses dragons hd
+do
+	$python monsters.py $file > $file
+	dos2unix $file
+done
