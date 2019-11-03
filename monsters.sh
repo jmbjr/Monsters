d52db7787dab3405c42f0818b1ef07763b744fe6
John Boyle <boylejm@gmail.com>
2019-11-02 21:31:57 -0700

    simple script to update monster outputs

---
+++ monsters.sh
@@ -0,0 +1,11 @@
+echo Updating monsters
+python=/c/ProgramData/Anaconda3/python.exe
+
+$python monsters.py mr > mr
+$python monsters.py hp > hp
+$python monsters.py attacks > attacks
+$python monsters.py resists > resists
+$python monsters.py ac > ac
+$python monsters.py speed > speed
+$python monsters.py defenses > defenses
+$python monsters.py dragons > dragons
